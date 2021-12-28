06f00d8d792d758ab8a5f0122c05df0bdcc2a3ba
Guilherme Zanini Moreira <guilherme.zm@outlook.com>
2021-12-28 17:50:55 -0300

    feat - inciado app backend frontend. Adicionado dockerfile

---
+++ backend/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+
+	r.Use(static.Serve("/", static.LocalFile("./web", true)))
+
+	api := r.Group("/api")
+
+	api.GET("/cities", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+}
